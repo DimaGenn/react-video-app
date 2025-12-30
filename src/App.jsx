f443b91abf4805c7f58d53d2f435817eb31002c7
Dmitry Sharapendo <dgsharapov@gmail.com>
2025-12-30 11:22:49 +0300

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Video } from './Video/Video'
+import './App.css'
+import { VIDEOS } from './videos'
+
+function App() {
+
+  return (
+
+    <div className="video-container">
+
+      {
+        VIDEOS.map((video) => (
+          <Video
+            key = {video.id}
+            title={video.title}
+            channelName={video.channelName}
+            img={video.img} />
+        ))
+      }
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/videos.js
@@ -0,0 +1,52 @@
+import reactLogo from './assets/react.svg';
+
+export const VIDEOS = [
+{
+    id: 1,
+    title: 'Видео 1',
+    channelName: 'youtube video',
+    img: reactLogo
+    },
+    {
+        id: 2,
+    title: 'Видео 2',
+    channelName: 'youtube video',
+    img: reactLogo
+    },
+    {
+        id: 3,
+    title: 'Видео 3',
+    channelName: 'youtube video',
+    img: reactLogo
+    },
+    {
+        id: 4,
+    title: 'Видео 4',
+    channelName: 'youtube video',
+    img: reactLogo
+    },
+    {
+        id: 5,
+    title: 'Видео 5',
+    channelName: 'youtube video',
+    img: reactLogo
+    },
+    {
+        id: 6,
+    title: 'Видео 6',
+    channelName: 'youtube video',
+    img: reactLogo
+    },
+    {
+        id: 7,
+    title: 'Видео 7',
+    channelName: 'youtube video',
+    img: reactLogo
+    },
+    {
+        id: 8,
+    title: 'Видео 8',
+    channelName: 'youtube video',
+    img: reactLogo
+    },
+]
